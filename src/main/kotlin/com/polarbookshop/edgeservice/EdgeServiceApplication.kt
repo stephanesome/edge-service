2add011d121e3d1e9b1ac72d2d6e289ddd105670
stephane <stephanesome@gmail.com>
2022-10-08 13:12:55 -0400

    Initial commit

---
+++ src/main/kotlin/com/polarbookshop/edgeservice/EdgeServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.polarbookshop.edgeservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EdgeServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<EdgeServiceApplication>(*args)
+}
